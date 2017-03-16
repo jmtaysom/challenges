64dbbe7173275a94aad977ad5332ddffd552459c
jmtaysom <james.taysom@gmail.com>
2017-03-15 20:25:49 -0400

    solved word value challenge

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,18 +1,27 @@
+import pytest
+
 from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     with open(DICTIONARY) as f:
-        return [line.strip('\n') for line in f]
+        return [line.strip('\n').replace('-','') for line in f]
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    return sum([LETTER_SCORES[letter.upper()] for letter in word])
 
-def max_word_value(words):
+def max_word_value(words=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    max_value = 0
+    best_word = ''
+    for word in words:
+        value = calc_word_value(word)
+        if value > max_value:
+            max_value = value
+            best_word = word
+    return best_word
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+    pytest.main()

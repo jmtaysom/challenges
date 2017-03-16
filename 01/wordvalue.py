2eb122799d966392b25c986f4a8804cd5136bbc6
jmtaysom <james.taysom@gmail.com>
2017-03-15 20:02:50 -0400

    added load words function

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,15 +1,15 @@
 from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
-    """Load dictionary into a list and return list"""
-    pass
+    with open(DICTIONARY) as f:
+        return [line.strip('\n') for line in f]
 
-def calc_word_value():
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
     pass
 
-def max_word_value():
+def max_word_value(words):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
     pass
